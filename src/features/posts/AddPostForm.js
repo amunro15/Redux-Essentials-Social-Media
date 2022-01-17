2941c8c0b195e95bc1f8792b68e4c4f352ef1a28
Amy Munro <amymunro15@gmail.com>
2022-01-17 10:42:33 -0500

    Hook up to server

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,32 +1,43 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { postAdded } from './postsSlice'
+import { addNewPost } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [userId, setUserId] = useState('')
+  const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
   const dispatch = useDispatch()
 
   // Selectors
   const users = useSelector(state => state.users)
 
+  // Handlers
   const onTitleChanged = e => setTitle(e.target.value)
   const onContentChanged = e => setContent(e.target.value)
   const onAuthorChanged = e => setUserId(e.target.value)
 
-  const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(postAdded(title, content, userId))
-
-      setTitle('')
-      setContent('')
-    }
-  }
-
   // Only allow to save post if includes title, content, userId
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
+  
+  const onSavePostClicked = async () => {
+    if (canSave) {
+      try {
+        setAddRequestStatus('pending')
+        // unwrap() provided by Redux and returns new Promise that has actual action.payload value from fulfilled action or throws error if rejected action
+        await dispatch(addNewPost({ title, content, user: userId })).unwrap();
+        setTitle('')
+        setContent('')
+        setUserId('')
+      } catch (err) {
+        console.error('Failed to save the post: ', err)
+      } finally {
+        setAddRequestStatus('idle')
+      }
+    }
+  };
 
   const usersOptions = users.map(user => (
     <option key={user.id} value={user.id}>
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,38 +1,69 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+// Components
 import { PostAuthor } from './PostAuthor';
 import { TimeAgo } from './TimeAgo';
 import { ReactionButtons } from './ReactionButtons';
+import { Spinner } from '../../components/Spinner'
 
+// Redux
+import { selectAllPosts, fetchPosts } from './postsSlice'
+
+// Subcomponent
+const PostExcerpt = ({ post }) => {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
+}
+
+// Component
 export const PostsList = () => {
-  const posts = useSelector(state => state.posts)
-  // Sort posts in reverse chronological order by datetime string
-  const orderedPosts = posts?.slice().sort((a, b) => b.date.localeCompare(a.date))
+  const dispatch = useDispatch();
 
-  const renderedPosts = orderedPosts.map(post => {
-    return (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <div>
-          <PostAuthor userId={post.user} />
-          <TimeAgo timestamp={post.date} />
-        </div>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
+  const posts = useSelector(selectAllPosts)
+  const postStatus = useSelector(state => state.posts.status)
+  const error = useSelector(state => state.posts.error)
 
-        <ReactionButtons post={post} />
-        <Link to={`/posts/${post.id}`} className="button muted-button">
-          View Post
-        </Link>
-      </article>
-    )
-  })
+  // If were to call fetchPosts on cdm, or when switched between views, we might end up fetching the posts several times
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [postStatus, dispatch])
+
+  let content;
+
+  if (postStatus === 'loading') {
+    content = <Spinner text="Loading..." />
+  } else if (postStatus === 'succeeded') {
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map(post => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
